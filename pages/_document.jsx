4d77a29c75bf750ac96d4511254cb8e6c93cbe72
Usama Tufail <miansamusama@gmail.com>
2023-07-15 03:05:40 +0500

    style: :art: update favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/logo.ico" sizes="any" />
+        <link rel="icon" href="/logo1.ico" sizes="any" />
       </Head>
       <body>
         <Main />
